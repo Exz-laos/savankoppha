ee48568c7ec1ae6c308f3c98892811feb1507a80
Exz-Laos <xbecomputer12345@gmail.com>
2024-09-08 22:02:39 +0900

    C:/Program Files/Git/

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,104 +1,105 @@
 
 
-const backendDomain=process.env.REACT_APP_BACKEND_URL 
-
-
-//"http://localhost:8080"
+const backendDomain = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080"
 
 const SummaryApi ={
     signUp : {
-        url : `${backendDomain}/api/signup` ,
+        url : `${backendDomain}api/signup` ,
         method: "post"
     },
     signIn : {
-        url : `${backendDomain}/api/signin` ,
+        url : `${backendDomain}api/signin` ,
         method: "post"
     },
     current_user : {
-        url : `${backendDomain}/api/user-details` ,
+        url : `${backendDomain}api/user-details` ,
         method: "get"
     },
     logout_user: {
-        url : `${backendDomain}/api/userLogout` ,
+        url : `${backendDomain}api/userLogout` ,
         method: "get"
     },
     allUser : {
-        url : `${backendDomain}/api/all-user` ,
+        url : `${backendDomain}api/all-user` ,
         method: "get"
     },
     updateUser : {
-        url : `${backendDomain}/api/update-user` ,
+        url : `${backendDomain}api/update-user` ,
         method: "post"
     },
     uploadProduct : {
-        url : `${backendDomain}/api/upload-product` ,
+        url : `${backendDomain}api/upload-product` ,
         method: "post"
     },
     allProduct : {
-        url : `${backendDomain}/api/get-product` ,
+        url : `${backendDomain}api/get-product` ,
         method: "get"
     },
     updateProduct : {
-        url : `${backendDomain}/api/update-product` ,
+        url : `${backendDomain}api/update-product` ,
         method: "post"
     },
     categoryProduct : {
-        url : `${backendDomain}/api/get-categoryProduct` ,
+        url : `${backendDomain}api/get-categoryProduct` ,
         method: "get"
     },
     categoryWiseProduct : {
-        url : `${backendDomain}/api/category-product` ,
+        url : `${backendDomain}api/category-product` ,
         method: "post"
     },
     productDetails : {
-        url : `${backendDomain}/api/product-details` ,
+        url : `${backendDomain}api/product-details` ,
         method: "post"
     },
     addToCartProduct : {
-        url : `${backendDomain}/api/addtocart` ,
+        url : `${backendDomain}api/addtocart` ,
         method: "post"
     },
     addToCartProductCount : {
-        url : `${backendDomain}/api/countAddToCartProduct` ,
+        url : `${backendDomain}api/countAddToCartProduct` ,
         method: "get"
     },
     addToCartProductView : {
-        url : `${backendDomain}/api/view-card-product` ,
+        url : `${backendDomain}api/view-card-product` ,
         method: "get"
     },
     updateCartProduct : {
-        url : `${backendDomain}/api/update-cart-product` ,
+        url : `${backendDomain}api/update-cart-product` ,
         method: "post"
     },
     deleteCartProduct : {
-        url : `${backendDomain}/api/delete-cart-product` ,
+        url : `${backendDomain}api/delete-cart-product` ,
         method: "post"
     },
     searchProduct : {
-        url : `${backendDomain}/api/search` ,
+        url : `${backendDomain}api/search`,
         method: "get"
     },
     filterProduct : {
-        url : `${backendDomain}/api/filter-product` ,
+        url : `${backendDomain}api/filter-product` ,
         method: "post"
     },
 
     deleteProduct : {
-        url : `${backendDomain}/api/delete-product` ,
+        url : `${backendDomain}api/delete-product` ,
         method: "DELETE"
     },
     uploadPaymentForm: {
-        url : `${backendDomain}/api/uploadPaymentForm` ,
+        url : `${backendDomain}api/uploadPaymentForm` ,
         method: "post"
     },
     allOrders: {
-        url: `${backendDomain}/api/all-orders`,
+        url: `${backendDomain}api/all-orders`,
         method: "GET"
     },
     updateOrderStatus: {
-        url: (orderId, status) => `${backendDomain}/api/orders/${orderId}/${status}`,
+        url: (orderId, status) => `${backendDomain}api/orders/${orderId}/${status}`,
         method: 'PUT'
     },
+
+
+
+
     
 }
 
